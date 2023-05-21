3beddf8e506b27ca987be4f513ff0d01a35c9c86
Arman <armanzakaryan0@gmail.com>
2023-05-21 21:36:28 +0400

    fix

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,20 +1,25 @@
 import {getCurrentDatetime} from './get-current-datetime.js'
 
 export const logger = async (req, res, next) => {
-    console.log(`${
-        getCurrentDatetime()
-    } [${
-        (req.header('x-forwarded-for') || '').padStart(15, ' ')
-    }] ${
-        (req.method || '').padStart(5, ' ')
-    } ${
-        (req.header('host') || '').padStart(25, ' ')
-    } ${
-        req.url
-    } ${
-        JSON.stringify(req.query)
-    } ${
-        JSON.stringify(req.body)
-    }`)
+  console.log(`${
+    getCurrentDatetime()
+  } [${
+    (req.header('x-forwarded-for') || '').padStart(15, ' ')
+  }] ${
+    (req.method || '').padStart(5, ' ')
+  } ${
+    (req.header('host') || '').padStart(25, ' ')
+  } ${
+    req.url
+  } ${
+    JSON.stringify(req.query)
+  } ${
+    JSON.stringify(req.body)
+  }`)
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+  } else {
     next()
+  }
 }
