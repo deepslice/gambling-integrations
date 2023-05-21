7c70587ea899a98cc0f6b40de2393f04d2d4d8f3
Arman <armanzakaryan0@gmail.com>
2023-05-21 22:02:10 +0400

    fix

--- src/wallet/handlers/middleware.js
+++ src/wallet/handlers/middleware.js
@@ -49,7 +49,11 @@ export async function middleware(req, res, next) {
     const secretKey = project.configs.secretKey
     const operatorId = project.configs.operatorId
 
-    const secretToken = crypto.createHash('md5').update(`${secretKey}` + `/` + `${req.originalUrl}`).digest('hex')
+    const secret = `${secretKey}` + `/` + `${req.originalUrl}`
+
+    console.log('secret--------',secret)
+
+    const secretToken = crypto.createHash('md5').update(secret).digest('hex')
 
     if (Number(operatorId) !== Number(req.query.operatorId)) {
       res.status(500).end()
