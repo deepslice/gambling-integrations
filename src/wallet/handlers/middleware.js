90f832c05a7d1dcf365565a0b4b53d5b61b1dda7
Arman <armanzakaryan0@gmail.com>
2023-05-21 21:04:31 +0400

    fix

--- src/wallet/handlers/middleware.js
+++ src/wallet/handlers/middleware.js
@@ -1,6 +1,7 @@
 import crypto from 'node:crypto'
 import mysql2 from 'mysql2/promise'
 import {getRedisClient} from '../../utils/redis.js'
+import {getCurrentDatetime} from '../../utils/get-current-datetime.js'
 
 /**
  * @type {mysql2.Pool}
@@ -16,55 +17,60 @@ const pool = mysql2.createPool({
 })
 
 export async function middleware(req, res, next) {
+  try {
+    console.log('---------------start middleware')
+    const token = req.query.token
+    const authorization = req.headers['authorization']
 
-  console.log('---------------start middleware')
-  const token = req.query.token
-  const authorization = req.headers['authorization']
+    const client = await getRedisClient()
 
-  const client = await getRedisClient()
+    const data = await client.get(`aspect-initial-token:${token}`).then(JSON.parse)
 
-  const data = await client.get(`aspect-initial-token:${token}`).then(JSON.parse)
+    if (!data) {
+      const response = {
+        error: 'Invalid Token',
+        errorCode: 1002,
+      }
+      res.status(200).json(response).end()
+      console.error('token--')
+      return
+    }
 
-  if (!data) {
+    const prefix = data.prefix
+
+    const [[project]] = await pool.query(`
+        select cast(configs as json) as configs
+        from casino.aspect_configs
+        where prefix = ?
+    `, [prefix])
+
+    const secretKey = project.configs.secretKey
+    const operatorId = project.configs.operatorId
+
+    const secretToken = crypto.createHash('md5').update(`${secretKey}` + `${req.originalUrl}`).digest('hex')
+
+    if (operatorId !== req.query.operatorId) {
+      res.status(500).end()
+      console.error('middleware operatorId')
+      return
+    }
+
+    if (`AUTH ${secretToken}` === authorization) {
+      next()
+      console.log('next-------')
+      return
+    }
+
     const response = {
       error: 'Invalid Token',
       errorCode: 1002,
     }
+
     res.status(200).json(response).end()
-    console.error('token--')
-    return
+    console.error('AUTH')
+  } catch (e) {
+    console.error(getCurrentDatetime(), e)
   }
+  res.status(500).json({message: 'internal server error'}).end()
 
-  const prefix = data.prefix
-
-  const [[project]] = await pool.query(`
-      select cast(configs as json) as configs
-      from casino.aspect_configs
-      where prefix = ?
-  `, [prefix])
-
-  const secretKey = project.configs.secretKey
-  const operatorId = project.configs.operatorId
-
-  const secretToken = crypto.createHash('md5').update(`${secretKey}` + `${req.originalUrl}`).digest('hex')
-
-  if (operatorId !== req.query.operatorId) {
-    res.status(500).end()
-    console.error('middleware operatorId')
-    return
-  }
-
-  if (`AUTH ${secretToken}` === authorization) {
-    next()
-    console.log('next-------')
-    return
-  }
-
-  const response = {
-    error: 'Invalid Token',
-    errorCode: 1002,
-  }
-
-  res.status(200).json(response).end()
-  console.error('AUTH')
 }
