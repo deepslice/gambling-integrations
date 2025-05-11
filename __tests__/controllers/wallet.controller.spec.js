1c01d4256a9c78d9684154d185cabf8d8d3547d7
dydx <denis@tonbet.fun>
2025-05-11 18:59:42 +0400

    feat(#test-task): work in progress

--- __tests__/controllers/wallet.controller.spec.js
+++ __tests__/controllers/wallet.controller.spec.js
@@ -3,16 +3,32 @@ import {describe, expect, it, jest} from '@jest/globals'
 import {AuthSessionService} from '#app/modules/auth/auth-session.service'
 import app from '#app/app'
 
-AuthSessionService.setSessionToken = jest.fn()
-AuthSessionService.validateSessionToken = jest.fn(async (token) => false)
-
 describe('GET /api/v1/wallet/balances', () => {
   it('should be authenticated', async () => {
+    const token = '12345'
+    AuthSessionService.validateSessionToken = jest.fn(
+      async (token) => true,
+    )
+
     const response = await request(app)
       .get('/api/v1/wallet/balances')
-      .set('Authorization', 'AUTH 12345')
+      .set('Authorization', `AUTH ${token}`)
 
-    expect(AuthSessionService.validateSessionToken).toBeCalled()
+    expect(AuthSessionService.validateSessionToken).toBeCalledWith(token)
+    expect(response.status).toBe(200)
+  })
+
+  it('should be rejected', async () => {
+    const token = '12345'
+    AuthSessionService.validateSessionToken = jest.fn(
+      async (token) => false,
+    )
+
+    const response = await request(app)
+      .get('/api/v1/wallet/balances')
+      .set('Authorization', `AUTH ${token}`)
+
+    expect(AuthSessionService.validateSessionToken).toBeCalledWith(token)
     expect(response.status).toBe(403)
   })
 })
