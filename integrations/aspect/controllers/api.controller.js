f0552bc513779e8f6a26d67e09b98e70590366f3
denis.kozlov <denis.kozlov@skillex.am>
2025-05-21 20:10:10 +0400

    feat(#test-task): WIP

--- integrations/aspect/controllers/api.controller.js
+++ integrations/aspect/controllers/api.controller.js
@@ -3,7 +3,6 @@ import {gameInitApi} from '#app/services/api/game-init.api'
 export class ApiController {
 
   async gameInit(req, res) {
-    console.log('req.body:', req.body)
     const response = await gameInitApi.gameInit(req.body)
     res.status(200).json(response).end()
   }
