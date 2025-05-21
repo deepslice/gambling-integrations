244a9588e99253ec9aaf468a5a09b68516add423
denis.kozlov <denis.kozlov@skillex.am>
2025-05-21 21:35:31 +0400

    feat(#test-task): WIP

--- scripts/game-init.sh
+++ scripts/game-init.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-GAME_ID=1
-USER_ID=1
-PREFIX=test
+GAME_ID=789
+USER_ID=565
+PREFIX="Sushi..."
 WAGERING_ID=0
 
-curl -kX 'http://localhost:3000/aspect/api/game-init' \
--h 'Content-Type: application/json' \
--d '{
-    "gameId": ${GAME_ID},
-    "userId": ${USER_ID},
-    "prefix": "${PREFIX}",
-    "wageringBalanceId": ${WAGERING_ID}
-}'
+curl -k -X POST 'http://localhost:3000/aspect/api/game-init' \
+-H 'Content-Type: application/json' \
+-d @- <<EOF
+{
+    "gameId": $GAME_ID,
+    "userId": $USER_ID,
+    "prefix": "$PREFIX",
+    "wageringBalanceId": $WAGERING_ID
+}
+EOF
--- scripts/get-balance.sh
+++ scripts/get-balance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TOKEN=<token>
-GAME_ID=<game-id>
+TOKEN="aspect-initial-token:3f7731a2ae152b2894476649ed14d6f51b3a2c15bb503e34d6560045a6855f97c2056558"
+GAME_ID=789
 
-curl -kX 'http://localhost:3000/api/v1/wallet/balances?token=${TOKEN}&gameId=${GAME_ID}'
+curl -k "http://localhost:3000/aspect/wallet/balances?token=${TOKEN}&gameId=${GAME_ID}"
